76fa5330e43f2715f90915e8813e70b840936bd3
moontai0724 <moontai0724@gmail.com>
2024-11-02 17:35:22 +0800

    build: use esbuild and add compitability for browser

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,33 +1,29 @@
 import { resolve } from "node:path";
 
-import { defineConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
-import viteConfig from "./vite.config";
-
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    resolve: {
-      alias: {
-        "~": resolve(import.meta.dirname, "tests"),
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(import.meta.dirname, "src"),
+      "~": resolve(import.meta.dirname, "tests"),
     },
-    test: {
-      coverage: {
-        enabled: true,
-        include: ["src/**/*.ts"],
-        reporter: ["text", "text-summary", "json", "html", "cobertura"],
-        reportOnFailure: true,
-      },
-      outputFile: {
-        junit: "./coverage/junit-report.xml",
-      },
-      passWithNoTests: true,
-      reporters: ["default", "junit"],
-      typecheck: {
-        checker: "tsc",
-        tsconfig: "./tests/tsconfig.json",
-      },
+  },
+  test: {
+    coverage: {
+      enabled: true,
+      include: ["src/**/*.ts"],
+      reporter: ["text", "text-summary", "json", "html", "cobertura"],
+      reportOnFailure: true,
     },
-  }),
-);
+    outputFile: {
+      junit: "./coverage/junit-report.xml",
+    },
+    passWithNoTests: true,
+    reporters: ["default", "junit"],
+    typecheck: {
+      checker: "tsc",
+      tsconfig: "./tests/tsconfig.json",
+    },
+  },
+});
